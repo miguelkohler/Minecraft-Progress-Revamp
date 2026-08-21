ffa61f75203900aa1f21cdb7a6d7c4eb31ab3515
miguelkohler <miguelkohler05@gmail.com>
2026-08-20 23:51:47 -0300

    yoooo now theres a rock and an axe(that dont do anything for now)

--- src/main/java/net/saitamaking/minecraftprogressrevamp/ProgressRevamp.java
+++ src/main/java/net/saitamaking/minecraftprogressrevamp/ProgressRevamp.java
@@ -1,5 +1,6 @@
 package net.saitamaking.minecraftprogressrevamp;
 
+import net.saitamaking.minecraftprogressrevamp.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -47,6 +48,8 @@ public class ProgressRevamp {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -60,6 +63,12 @@ public class ProgressRevamp {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.LOOSEPEBBLE);
+        }
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES){
+            event.accept(ModItems.PRIMITIVEAXE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
